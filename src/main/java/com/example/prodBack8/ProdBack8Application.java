1444d031814265e80fb82e7c4960165e9488f4b0
TimofeyNov <timn2020@inbox.ru>
2025-10-17 15:51:23 +0300

    initial commit

---
+++ src/main/java/com/example/prodBack8/ProdBack8Application.java
@@ -0,0 +1,13 @@
+package com.example.prodBack8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdBack8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdBack8Application.class, args);
+	}
+
+}
